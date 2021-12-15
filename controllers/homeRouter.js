321665bb7319ebd5496ba15bae43aa2353eedc2b
David Silveira <daviewave@gmail.com>
2021-12-14 21:45:09 -0500

    added code to homeRouter to get all the posts on the homepage

---
+++ controllers/homeRouter.js
@@ -0,0 +1,40 @@
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { Post, User, Comment } = require("../models");
+
+//gets all homepage posts
+router.get("/", (req, res) => {
+  console.log("\n======================");
+  Post.findAll({
+    attributes: ["id", "title", "post_message", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((db_posts) => {
+      const posts = db_posts.map((currentPost) =>
+        currentPost.get({ plain: true })
+      );
+
+      res.render("homepage", {
+        posts,
+        loggedIn: req.session.loggedIn,
+        username: req.session.username,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
