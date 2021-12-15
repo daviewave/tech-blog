800535bf4d2225965b05f3c34af34efc83459840
David Silveira <daviewave@gmail.com>
2021-12-14 20:25:18 -0500

    added user-seeds file to seeds folder

---
+++ seeds/user-seeds.js
@@ -0,0 +1,34 @@
+const sequalize = require("sequelize");
+const { User, Post } = require("../models");
+
+const users = [
+  {
+    username: "flufflykitten21",
+    email: "fluffkitt@gmail.com",
+    password: "pass12345",
+  },
+  {
+    username: "tigerking",
+    email: "tigerking@gmail.com",
+    password: "pass12345",
+  },
+  {
+    username: "batman",
+    email: "batman@gmail.com",
+    password: "pass12345",
+  },
+  {
+    username: "brendaneich",
+    email: "brave@brave.com",
+    password: "pass12345",
+  },
+  {
+    username: "attentiontoken",
+    email: "attentiontoken@gmail.com",
+    password: "pass12345",
+  },
+];
+
+const userSeeds = () => User.bulkCreate(userdata, { individualHooks: true });
+
+module.exports = userSeeds;
