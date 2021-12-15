748b8350e556fe3955f11a29100cbe6c4d90187d
David Silveira <daviewave@gmail.com>
2021-12-14 21:52:30 -0500

    added rest of code for homeRouter and added auth.js within the util folder to deal with login authorizations

--- controllers/homeRouter.js
+++ controllers/homeRouter.js
@@ -45,6 +45,49 @@ router.get("/login", (req, res) => {
     res.redirect("/");
     return;
   }
-
   res.render("login");
 });
+
+//gets a post depending on the ID specified
+router.get("/post/:id", (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "title", "post_message", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((db_posts) => {
+      if (!db_posts) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+
+      const currentPost = db_posts.get({ plain: true });
+
+      res.render("single-post", {
+        currentPost,
+        loggedIn: req.session.loggedIn,
+        username: req.session.username,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
