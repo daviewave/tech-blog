32db554a02e5282ce9e5d3770a4ca5142b89dab7
David Silveira <daviewave@gmail.com>
2021-12-14 19:14:25 -0500

    got sequalize to establish connection to the mysql2 server and database

--- server.js
+++ server.js
@@ -18,3 +18,15 @@ const sess = {
     db: sequalize,
   }),
 };
+
+app.use(session(sess));
+
+app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+sequalize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now listening"));
+});
